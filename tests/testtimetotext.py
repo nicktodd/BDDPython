9bab30ea4af70aad21f244d80527cf06979046da
Nick Todd <nick.todd@conygre.com>
2023-01-25 10:53:22 +0000

    minor fixes to test

--- tests/testtimetotext.py
+++ tests/testtimetotext.py
@@ -4,7 +4,6 @@ from timetotext import TimeToText
 
 class TestStringMethods(unittest.TestCase):
 
-    time_to_text = None
 
     def setUp(self):
         self.time_to_text=  TimeToText()
@@ -14,8 +13,8 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual("midnight", result)
 
     def test_midday(self):
-        result = self.time_to_text.get_time_as_text(time(0,0,0))
-        self.assertEqual("midnight", result)
+        result = self.time_to_text.get_time_as_text(time(12,0,0))
+        self.assertEqual("midday", result)
 
 
 if __name__ == '__main__':
